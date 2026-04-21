6588dbd1b8c84d874c957d80ef29060e67dd60ab
rubenromero17 <ruromeroaponte@safareyes.es>
2026-04-21 13:37:38 +0200

    Creamos primer modelo

---
+++ backend/src/main/java/com/example/backend/models/Usuarios.java
@@ -0,0 +1,15 @@
+package com.example.backend.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+#
+public class Usuarios {
+
+}
