34b1deb9584c5e237f0ff0768c7445c9824ca445
rubenromero17 <ruromeroaponte@safareyes.es>
2026-04-29 12:45:30 +0200

    Creamos el service de alumno

---
+++ backend/src/main/java/com/example/backend/services/AlumnosServices.java
@@ -0,0 +1,12 @@
+package com.example.backend.services;
+
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Service
+public class AlumnosServices {
+
+
+}
